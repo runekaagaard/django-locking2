3a4e1c1d2f57019911fa3fcf0a9c13dbdba8051f
Rob Combs <robert.combs@cmgdigital.com>
2011-12-19 13:18:47 -0500

    Add an expiration warning flag.

--- locking/__init__.py
+++ locking/__init__.py
@@ -4,6 +4,10 @@ from django.conf import settings
 import logging
 import urls
 
+#TODO: LOCK_TIMEOUT isn't being used anymore, clean it up throughout the code
 LOCK_TIMEOUT = getattr(settings, 'LOCK_TIMEOUT', 1800)
 
 logger = logging.getLogger('django.locker')
+
+time_until_expiration = int(settings.LOCKING['time_until_expiration'])
+time_until_warning = settings.LOCKING['time_until_warning']
