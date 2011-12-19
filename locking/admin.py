fa121ea347dbfbea5510c2bb201d82a8e86932aa
Rob Combs <robert.combs@cmgdigital.com>
2011-12-19 15:48:58 -0500

    Ease installation by making the change list lock column a part of the LockAdmin class.

--- locking/admin.py
+++ locking/admin.py
@@ -49,45 +49,44 @@ class LockableAdmin(admin.ModelAdmin):
             pass
 
 
-def get_lock_for_admin(self_obj, obj):
-    ''' 
-    returns the locking status along with a nice icon for the admin interface 
-    use in admin list display like so: list_display = ['title', 'get_lock_for_admin']
-    '''
+    def get_lock_for_admin(self_obj, obj):
+        ''' 
+        returns the locking status along with a nice icon for the admin interface 
+        use in admin list display like so: list_display = ['title', 'get_lock_for_admin']
+        '''
 
-    locked_by = ''
-    class_name = 'unlocked'
-    output = ''
+        locked_by = ''
+        class_name = 'unlocked'
+        output = ''
 
-    content_type = ContentType.objects.get_for_model(obj)
-    try:
-        lock = Lock.objects.get(entry_id=obj.id, app=content_type.app_label, model=content_type.model)
-        class_name = 'locked'
-        locked_by = lock.locked_by.username
-        output = str(obj.id)
-    except Lock.DoesNotExist:
-        return ''
+        content_type = ContentType.objects.get_for_model(obj)
+        try:
+            lock = Lock.objects.get(entry_id=obj.id, app=content_type.app_label, model=content_type.model)
+            class_name = 'locked'
+            locked_by = lock.locked_by.username
+            output = str(obj.id)
+        except Lock.DoesNotExist:
+            return ''
 
-    if lock.is_locked:
-        seconds_remaining = lock.lock_seconds_remaining
-        minutes_remaining = seconds_remaining/60
-        locked_until = _("Still locked for %s more minute(s) by %s.") \
-            % (minutes_remaining, lock.locked_by)
-        if self_obj.request.user == lock.locked_by: 
-            locked_until_self = _("You have a lock on this content for %s more minute(s).") \
-                % (minutes_remaining)
-            locked_until = '<img src="%slocking/img/page_edit.png" title="%s" />' \
-                % (settings.MEDIA_URL, locked_until_self)
-        else:
+        if lock.is_locked:
+            seconds_remaining = lock.lock_seconds_remaining
+            minutes_remaining = seconds_remaining/60
             locked_until = _("Still locked for %s more minute(s) by %s.") \
                 % (minutes_remaining, lock.locked_by)
-            locked_until = '<img src="%slocking/img/lock.png" title="%s" />' \
-                % (settings.MEDIA_URL, locked_until)
-        full_name = "%s %s" % (lock.locked_by.first_name,lock.locked_by.last_name)
-        return u'<a href="#" id=%s class="lock-status %s" title="Locked By: %s">%s%s</a>' % (output, class_name, full_name, locked_until, " " + locked_by)
-    else:
-        return ''
+            if self_obj.request.user == lock.locked_by: 
+                locked_until_self = _("You have a lock on this content for %s more minute(s).") \
+                    % (minutes_remaining)
+                locked_until = '<img src="%slocking/img/page_edit.png" title="%s" />' \
+                    % (settings.MEDIA_URL, locked_until_self)
+            else:
+                locked_until = _("Still locked for %s more minute(s) by %s.") \
+                    % (minutes_remaining, lock.locked_by)
+                locked_until = '<img src="%slocking/img/lock.png" title="%s" />' \
+                    % (settings.MEDIA_URL, locked_until)
+            full_name = "%s %s" % (lock.locked_by.first_name,lock.locked_by.last_name)
+            return u'<a href="#" id=%s class="lock-status %s" title="Locked By: %s">%s%s</a>' % (output, class_name, full_name, locked_until, " " + locked_by)
+        else:
+            return ''
 
-
-get_lock_for_admin.allow_tags = True
-get_lock_for_admin.short_description = 'Lock'
+    get_lock_for_admin.allow_tags = True
+    get_lock_for_admin.short_description = 'Lock'
