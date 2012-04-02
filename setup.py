95ecf6398eff9bf416e99c6de117fd9d272b6767
Rob Combs <robert.combs@cmgdigital.com>
2012-04-02 14:41:12 -0400

    Updated setup with the README.md extension.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 version = '0.3.0'
-README = os.path.join(os.path.dirname(__file__), 'README')
+README = os.path.join(os.path.dirname(__file__), 'README.md')
 long_description = open(README).read()
 setup(name='django-locking',
       version=version,
